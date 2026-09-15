64d5927f31dc33d77c65bea005ca40cfb01767b0
David ifeanyi <davidifeanyicelestine586@gmail.com>
2026-09-15 09:45:01 +0000

    refine: load layout rhythm refinements

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./typography.css";
 import "./color-refinement.css";
+import "./layout-refinement.css";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://david-ifeanyi-pi.vercel.app";
 
